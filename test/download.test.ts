3b853997d160767440642ea7dee8d0df0b9e60e4
Vincent-the-gamer <erxianqiaottjt@163.com>
2025-11-22 22:20:05 +0800

    chore: update deps.

--- test/download.test.ts
+++ test/download.test.ts
@@ -1,15 +1,11 @@
-import { it } from 'vitest'
 import { downloadVideo, remoteM3U8ToMP4 } from '../src'
 
-it('download remote m3u8 to local mp4', async () => {
-  await remoteM3U8ToMP4({
-    url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-  })
+await remoteM3U8ToMP4({
+  url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
 })
 
-it('download mp4', async () => {
-  await downloadVideo({
-    url: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
-    path: "./big_buck_bunny.mp4"
-  })
+await downloadVideo({
+  url: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+  path: "./big_buck_bunny.mp4"
 })
+
